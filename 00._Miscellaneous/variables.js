802452bfc0445e4799e124f217a6d531e075cf93
Emiliobocadillo <82929546+Emiliobocadillo@users.noreply.github.com>
2023-02-03 10:35:37 +0100

    Created the variables file

---
+++ 00._Miscellaneous/variables.js
@@ -0,0 +1,9 @@
+// Const rule 1: have to assign a value when declaring
+// Const me; 
+
+const me = {name: "Hans"};
+
+// Const rule 2: Cannot reassign 
+me.name = "Emil" // key-valu pairs
+
+console.log(me);
