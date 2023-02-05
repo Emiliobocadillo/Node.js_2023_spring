16055d189613a0a0e8d7bfb2fba71be90d9cbad1
Emiliobocadillo <82929546+Emiliobocadillo@users.noreply.github.com>
2023-02-05 15:00:07 +0100

    Added all the exercise files

--- 00._Miscellaneous/variables.js
+++ 00._Miscellaneous/variables.js
@@ -1,9 +1,27 @@
-// Const rule 1: have to assign a value when declaring
-// Const me; 
+// Const Rule 1: Have to assign a value when declaring.  
+// const me;
 
-const me = {name: "Hans"};
+const me = {
+    /* key: value 
+    === key-value pairs
+    */
+   name: "Hans"
+};
+const hobbies = ["Football", "Swimming"];
+hobbies.push("Cooking");
+me.hobbies = hobbies;
 
-// Const rule 2: Cannot reassign 
-me.name = "Emil" // key-valu pairs
+
+// Const Rule 2: Cannot reassign to constant
+// me = {};
+me.name = "Johannes";
+
+
 
 console.log(me);
+
+
+const hobbyOne = "Football skill level: '6'";
+const hobbyTwo = 'Swimming skill level: "3"';
+const hobbyThree = `Cooking skill level: "'${2 + 2}"'`;
+console.log(hobbyThree);
